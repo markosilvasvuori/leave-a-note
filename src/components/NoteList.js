22f7fd8974a56e1b3640e203eb56f7f4d98eccbe
Marko Silvasvuori <marko.silvasvuori@gmail.com>
2021-11-19 12:52:33 +0200

    Added loading and error messages to JSX return

--- src/components/NoteList.js
+++ src/components/NoteList.js
@@ -5,12 +5,14 @@ import Note from './Note';
 import classes from './NoteList.module.css';
 
 const NoteList = (props) => {
-    const {notes} = useContext(NoteContext);
+    const {notes, isLoading, error} = useContext(NoteContext);
 
     return (
         <ul className={classes["note-list"]}>
             {!notes && <p>Be the first one to leave a note!</p>}
-            {notes && Object.values(notes).reverse().map((note) => (
+            {isLoading && !error.isError && <p>Loading...</p>}
+            {error.isError && <p>{error.errorMessage}</p>}
+            {!isLoading && !error.isError && notes && Object.values(notes).reverse().map((note) => (
                 <Note 
                     key={note.id}
                     id={note.id}
